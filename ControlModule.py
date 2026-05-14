b6f2d2b1f7bc0390ad4bd38ca81e5e4d5d3163d8
100589912 <100589912@alumnos.uc3m.es>
2026-05-14 18:35:03 +0200

    P and C functions added

--- ControlModule.py
+++ ControlModule.py
@@ -8,16 +8,55 @@ class ControlModule:
         pass
 
     @staticmethod
-    def generate_P() -> np.ndarray:
+    def generate_P(prob_tables,n_states) -> np.ndarray:
         """ Function that generates the probabilities (transition) matrix """
         ### TO BE COMPLETED BY THE STUDENTS ###
         ...
+        P = np.zeros((3, n_states, n_states))
+        deltas = {
+            "decrease": [-2, -1, 0],
+            "maintain": [-1, 0, +1],
+            "increase": [0, +1, +2]
+        }
+        for a_idx, action in enumerate(["decrease", "maintain", "increase"]):
+            table = prob_tables[action]        
+            action_dts = deltas[action]
+            for s in range(n_states):
+                for delta, p in zip(action_dts, table):
+                    next_s = s + delta
+                    if next_s < 0:
+                        next_s = 0
+                    elif next_s >= n_states:
+                        next_s = n_states - 1
+                    P[a_idx, s, next_s] += p
+        return P
 
     @staticmethod
-    def generate_R() -> np.ndarray:
+    def generate_R(demand,n_states) -> np.ndarray:
         """ Function that generates the rewards (costs) matrix """
         ### TO BE COMPLETED BY THE STUDENTS ###
         ...
+        R = np.zeros((3, n_states))
+        for s in range(n_states):
+            level = s / 100.0           
+            base_cost = abs(demand - level)
+            for a in range(3):
+
+                if a == 0:        # decrease
+                    direction = -1
+                elif a == 1:      # maintain
+                    direction = 0
+                else:             # increase
+                    direction = +1
+                # penalty 
+                if direction > 0 and demand < level:
+                    cost = base_cost * 2
+                elif direction < 0 and demand > level:
+                    cost = base_cost * 2
+                else:
+                    cost = base_cost
+                R[a, s] = -cost
+        return R
 
     @staticmethod
     def control_iteration() -> np.int32:
