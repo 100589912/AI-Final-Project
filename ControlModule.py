724caa1eb55fd74c75fc96e589484392646f9266
juliaj27 <juliajung27@berkeley.edu>
2026-05-14 16:50:54 -0700

    Update ControlModule.py

--- ControlModule.py
+++ ControlModule.py
@@ -68,7 +68,10 @@ class ControlModule:
     
         """ Function that computes one control-iteration """
         ### TO BE COMPLETED BY THE STUDENTS ###
-
+    solver = mdtoolbox.mdp.ValueIteration(P,R,gamma)
+    solver.run()
+    policy = np.array(solver.policy) 
+    return np.int32(policy[state]) 
 
     @staticmethod
     def control_loop(demand: np.ndarray, 
@@ -78,35 +81,35 @@ class ControlModule:
                      gamma: np.float64) -> np.ndarray:
         """ Function that computes all the required iterations (control-loop) to satisfy the power demand """
         ### TO BE COMPLETED BY THE STUDENTS ###
-prob_tables = {
-    "decrease": probs[0], 
-    "maintain": probs[1], 
-    "increase": probs[2]
-}
-P = ControlModule.generate_P(prob_tables = prob_tables, n_states=n_states) 
-response = np.zeros_like(a=demand, dtype=np.float64) 
-current_state = np.int32(round(demand[0] * (n_states - 1))) 
-movements = {
-    0:[-2,-1,0],
-    1:[-1,0,+1],
-    2:[0,+1,+2]
-} 
-for t in range(demand.shape[0]):
-    R = ControlModule.generate_R(demand=demand[t], n_states=n_states) 
-    action = ControlModule.control_iteration( 
-        P=P, 
-        R=R,
-        state=current_state, 
-        gamma=gamma
-    )
-    selected_movement = np.random.choice(
-        movements{int(action)],
-        p=probs[int(action)]
-    ) 
-    next_state = current_state + selected_movement
-    next_state= max(0, min(n_states -1, next_state))
-    current_state = np.int32(next_state) 
-    response[t] = current_state / 100.0
+        prob_tables = {
+            "decrease": probs[0], 
+            "maintain": probs[1], 
+            "increase": probs[2]
+        }
+        P = ControlModule.generate_P(prob_tables = prob_tables, n_states=n_states) 
+        response = np.zeros_like(a=demand, dtype=np.float64) 
+        current_state = np.int32(round(demand[0] * (n_states - 1))) 
+        movements = {
+            0:[-2,-1,0],
+            1:[-1,0,+1],
+            2:[0,+1,+2]
+        } 
+        for t in range(demand.shape[0]):
+        R = ControlModule.generate_R(demand=demand[t], n_states=n_states) 
+        action = ControlModule.control_iteration( 
+            P=P, 
+            R=R,
+            state=current_state, 
+            gamma=gamma
+        )
+        selected_movement = np.random.choice(
+            movements[int(action)],
+            p=probs[int(action)]
+        ) 
+        next_state = current_state + selected_movement
+        next_state= max(0, min(n_states -1, next_state))
+        current_state = np.int32(next_state) 
+        response[t] = current_state / 100.0
 return response 
                     
                          
