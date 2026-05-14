5e579b3e1f56e3dba86c3f291fde0c809217887a
juliaj27 <juliajung27@berkeley.edu>
2026-05-14 16:42:30 -0700

    Update ControlModule.py

--- ControlModule.py
+++ ControlModule.py
@@ -11,13 +11,14 @@ class ControlModule:
     def generate_P(prob_tables,n_states) -> np.ndarray:
         """ Function that generates the probabilities (transition) matrix """
         ### TO BE COMPLETED BY THE STUDENTS ###
-        ...
+        
         P = np.zeros((3, n_states, n_states))
         deltas = {
             "decrease": [-2, -1, 0],
             "maintain": [-1, 0, +1],
             "increase": [0, +1, +2]
         }
+
         for a_idx, action in enumerate(["decrease", "maintain", "increase"]):
             table = prob_tables[action]        
             action_dts = deltas[action]
@@ -35,8 +36,9 @@ class ControlModule:
     def generate_R(demand,n_states) -> np.ndarray:
         """ Function that generates the rewards (costs) matrix """
         ### TO BE COMPLETED BY THE STUDENTS ###
-        ...
+    
         R = np.zeros((3, n_states))
+    
         for s in range(n_states):
             level = s / 100.0           
             base_cost = abs(demand - level)
@@ -59,10 +61,14 @@ class ControlModule:
         return R
 
     @staticmethod
-    def control_iteration() -> np.int32:
+    def control_iteration(P: np.ndarray,
+                          R: np.ndarray,
+                          state: np.int32,
+                          gamma: np.float64)-> np.int32:
+    
         """ Function that computes one control-iteration """
         ### TO BE COMPLETED BY THE STUDENTS ###
-        ...
+
 
     @staticmethod
     def control_loop(demand: np.ndarray, 
@@ -72,7 +78,39 @@ class ControlModule:
                      gamma: np.float64) -> np.ndarray:
         """ Function that computes all the required iterations (control-loop) to satisfy the power demand """
         ### TO BE COMPLETED BY THE STUDENTS ###
-
-        ### DUMMY BEHAVIOUR TO PREVENT CRASHING (MUST BE DELETED AFTER THE FULL IMPLEMENTATION) ###
-        return np.zeros_like(a=demand, dtype=np.float64)
-        ### ###
+prob_tables = {
+    "decrease": probs[0], 
+    "maintain": probs[1], 
+    "increase": probs[2]
+}
+P = ControlModule.generate_P(prob_tables = prob_tables, n_states=n_states) 
+response = np.zeros_like(a=demand, dtype=np.float64) 
+current_state = np.int32(round(demand[0] * (n_states - 1))) 
+movements = {
+    0:[-2,-1,0],
+    1:[-1,0,+1],
+    2:[0,+1,+2]
+} 
+for t in range(demand.shape[0]):
+    R = ControlModule.generate_R(demand=demand[t], n_states=n_states) 
+    action = ControlModule.control_iteration( 
+        P=P, 
+        R=R,
+        state=current_state, 
+        gamma=gamma
+    )
+    selected_movement = np.random.choice(
+        movements{int(action)],
+        p=probs[int(action)]
+    ) 
+    next_state = current_state + selected_movement
+    next_state= max(0, min(n_states -1, next_state))
+    current_state = np.int32(next_state) 
+    response[t] = current_state / 100.0
+return response 
+                    
+                         
+                         
+                         
+                         
+        
